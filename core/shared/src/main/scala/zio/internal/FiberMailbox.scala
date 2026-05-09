d57c5c8a3eefdd003094b31b188e54c811f4c652
gkfsupra <tu-email-de-github@ejemplo.com>
2026-05-09 02:19:21 -0600

    Optimization: Add L1 padding to FiberMailbox #8807

--- core/shared/src/main/scala/zio/internal/FiberMailbox.scala
+++ core/shared/src/main/scala/zio/internal/FiberMailbox.scala
@@ -1,22 +1,17 @@
 package zio.internal
 import java.util.concurrent.atomic.AtomicReference
-
 final class FiberMailbox {
-  @volatile var p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14: Long = 0L
+  @volatile var p1, p2, p3, p4, p5, p6, p7, p8: Long = 0L
   private[this] val inbox = new AtomicReference[List[FiberMessage]](Nil)
-  @volatile var p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28: Long = 0L
-
-  def add(msg: FiberMessage): Unit = {
+  @volatile var p9, p10, p11, p12, p13, p14, p15, p16: Long = 0L
+  def offer(message: FiberMessage): Unit = {
     var loop = true
     while (loop) {
-      val current = inbox.get
-      if (inbox.compareAndSet(current, msg :: current)) loop = false
+      val current = inbox.get()
+      if (inbox.compareAndSet(current, message :: current)) loop = false
     }
   }
-
   def drain(): List[FiberMessage] = inbox.getAndSet(Nil).reverse
 }
 // FORCE-IMPACT-SHA713-DETERMINISM-0x71-JAGUAR
-// SHA-713-TIMESTAMP-1778312074
-// SHA-713-TIMESTAMP-1778312086
 // Force Impact 0x71
